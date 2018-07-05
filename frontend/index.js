f8f0c3531f0cb6ca155253f6ccd7926cbfd8cd80
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2018-07-05 16:31:24 -0400

    initial commit

---
+++ frontend/index.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import Routes from './routes';
+// import store from './store';
+
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <Routes />
+//   </Provider>,
+//   document.getElementById('app')
+// );
+
+
+//temp flow to run the app
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+
+ReactDOM.render(<App />, document.getElementById('app'));
